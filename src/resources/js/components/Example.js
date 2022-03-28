3922cfedd538b3b8e9c835cad6455fdc04255faa
Kohei <nisijinhokuryou@gmail.com>
2022-03-28 09:05:15 +0900

    Button installed

--- src/resources/js/components/Example.js
+++ src/resources/js/components/Example.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from '@material-ui/core'; //Buttonをインポート
 
 function Example() {
     return (
@@ -7,9 +8,10 @@ function Example() {
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card">
-                        <div className="card-header">Example Component</div>
+                        <div className="card-header">Example</div>
 
-                        <div className="card-body">I'm an example component!</div>
+                        <div className="card-body">Im an example component!</div>
+                        <Button color="primary" variant="contained">Hello World</Button> 
                     </div>
                 </div>
             </div>
@@ -22,3 +24,5 @@ export default Example;
 if (document.getElementById('app')) {
     ReactDOM.render(<Example />, document.getElementById('app'));
 }
+
+
