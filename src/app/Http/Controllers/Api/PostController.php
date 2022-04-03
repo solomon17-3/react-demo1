42f40334874f86221bac81a9ba6d98298199b09a
Kohei <nisijinhokuryou@gmail.com>
2022-04-03 15:12:46 +0900

    api routing

---
+++ src/app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Post; 
+
+class PostController extends Controller
+{
+    //postの一覧表示
+    public function index()
+    {
+        $posts = Post::all();
+        //postの一覧をjson形式で返却する
+        return responce()->json($posts, 200);
+    }
+}
--- src/routes/api.php
+++ src/routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//http://localhost/api/postsにアクセスしたとき下記URLにアクセス
+Route::group(['middleware' => 'api'], function(){
+    Route::get('posts', 'App\Http\Controllers\Api\PostController@index');
 });
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
